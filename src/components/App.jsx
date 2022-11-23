6faaf216a22320e6a17698909391df8a49e0c05b
YuriySamusew <6177385@gmail.com>
2022-11-23 22:02:08 +0200

    01 render componenet and PropTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { RecipeList } from 'components/RecipeList';
+import recipes from 'components/recipes.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <RecipeList recipes={recipes} />
+    </>
   );
 };
